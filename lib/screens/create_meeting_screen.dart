7277dd59ed78c9a5c56b721131566f1d533a8107
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2021-07-05 08:34:05 +0530

    join and create meeting added

--- lib/screens/create_meeting_screen.dart
+++ lib/screens/create_meeting_screen.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:teams/constants/variables.dart';
 
-class CreateMeetingScreen extends StatelessWidget {
+class CreateMeetingScreen extends StatefulWidget {
   const CreateMeetingScreen({Key? key}) : super(key: key);
 
   @override
+  State<CreateMeetingScreen> createState() => _CreateMeetingScreenState();
+}
+
+class _CreateMeetingScreenState extends State<CreateMeetingScreen> {
+  String code = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: Text(
+              "Create a code and share it with your friends",
+              style: myStyle(20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Code:",
+                style: myStyle(30),
+              ),
+              Text(
+                code,
+                style: myStyle(30, Colors.purple, FontWeight.w700),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          InkWell(
+            onTap: () {
+              setState(() {
+                // code = Uuid().v1().substring(0, 6);
+              });
+            },
+            child: Container(
+              width: width / 2,
+              height: 50,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFBBDEFB),
+                    Color(0xFF90CAF9),
+                    Color(0xFF64B5F6),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Create Code",
+                  style: myStyle(
+                    20,
+                    Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/join_meeting_screen.dart
+++ lib/screens/join_meeting_screen.dart
@@ -1,10 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:teams/constants/variables.dart';
 
-class JoinMeetingScreen extends StatelessWidget {
+import 'meet_screen.dart';
+
+class JoinMeetingScreen extends StatefulWidget {
   const JoinMeetingScreen({Key? key}) : super(key: key);
 
   @override
+  State<JoinMeetingScreen> createState() => _JoinMeetingScreenState();
+}
+
+class _JoinMeetingScreenState extends State<JoinMeetingScreen> {
+  String roomCode = "";
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Room code",
+                style: myStyle(20),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              PinCodeTextField(
+                appContext: context,
+                autoDisposeControllers: false,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(shape: PinCodeFieldShape.underline),
+                length: 6,
+                animationDuration: const Duration(
+                  milliseconds: 300,
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.length != 6) return "Enter the code properly";
+                  return "";
+                },
+                onChanged: (value) {
+                  roomCode = value;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                style: myStyle(20),
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: "Name (Leave if you want your username )",
+                  labelStyle: myStyle(15),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              InkWell(
+                onTap: () async {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MeetScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "Join Meeting",
+                      style: myStyle(20, Colors.white),
+                    ),
+                  ),
+                  width: double.maxFinite,
+                  height: 64,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFFBBDEFB),
+                        Color(0xFF90CAF9),
+                        Color(0xFF64B5F6),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
